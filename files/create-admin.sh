59093d4b40afb8dc13094c0a9dc6dbaadfc57f2d
Lucas Guilherme Diedrich <lucas.diedrich@unila.edu.br>
2018-08-07 15:59:10 -0300

    Add create-admin script.

--- files/create-admin.sh
+++ files/create-admin.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 
+V_NOME=$1
+V_CPF=$2
+V_EMAIL=$3
+
+if [[ -z $V_NOME || -z $V_CPF || -z $V_EMAIL ]]; then
+    echo "ERROR: O comando deve ser utilizado como abaixo:"
+    echo "create-admin 'NOME DO USUARIO' 'CPF' 'EMAIL'"
+    echo ""
+    exit 1
+fi
+
+# Perfis
+# 1. Comitê
+# 2. Subcomitê
+# 3. Núcleo (Administrador)
+# 4. Unidade
+# 5. Gestor do Processo
+# 6. Analista de Risco
+V_PERFIL=3
+
+psql -U $DB_USER -h $DB_HOST $DB_NAME -c "INSERT INTO gestaoriscos.tb_usuario VALUES (('nextval.sk_usuario'), '$V_NOME', '$V_CPF', null, '$V_EMAIL');"
+psql -U $DB_USER -h $DB_HOST $DB_NAME -c "INSERT INTO gestaoriscos.tb_permissao VALUES (('nextval.sk_permissao'), LASTVAL('sk_usuario'), $V_PERFIL , false, current_timestamp);"
