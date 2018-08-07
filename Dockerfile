312bc99c4a946f13befbbfcb6351c3174d866c32
Lucas Guilherme Diedrich <lucas.diedrich@unila.edu.br>
2018-08-07 15:02:42 -0300

    Primeiro commit em funcionamento.

---
+++ Dockerfile
@@ -0,0 +1,39 @@
+# FROM nginx:stable-alpine
+FROM openjdk:8-jdk-alpine
+LABEL maintainer="Lucas G. Diedrich <lucas.diedrich@gmail.com>"
+
+WORKDIR /
+
+# Postgres Opts
+ENV JAVA_OPTS="-server -Xmx2G -XX:MinHeapFreeRatio=20 -XX:MaxHeapFreeRatio=40 -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70" \
+    DB_CHECK="1" \
+    DB_HOST="agathadb" \
+    DB_NAME="agatha" \
+    DB_USER="gestaoriscos" \
+    DB_PASSWORD="gestaoriscos" \
+    ECIDADAO_ENV="PROD" \
+    ECIDADAO_SERVER="MINHAINSTITUICAO.EDU.BR" \
+    ECIDADAO_ID="MINHAID" \
+    ECIDADAO_SECRET="MINHAKEY" 
+
+COPY files/springconf/application.properties /config/
+COPY files/springconf/application.yaml /config/
+COPY files/springconf/jasperreports_extension.properties /config/
+COPY files/pre-init.sh /usr/bin/pre-init
+COPY files/create-admin.sh /usr/bin/create-admin
+COPY files/default.conf /etc/nginx/conf.d/
+COPY files/proxy.conf /etc/nginx/
+COPY files/supervisord.conf /etc/supervisord.conf
+
+RUN apk add --update --no-cache bash nginx supervisor postgresql-client tar curl && \
+    mkdir -p /usr/share/nginx/html/ /run/nginx/ /run/supervisord/ /config && \
+    curl https://softwarepublico.gov.br/gitlab/agatha/agatha/raw/master/docker/spring/app.jar --output /app.jar && \
+    curl https://softwarepublico.gov.br/gitlab/agatha/agatha/raw/master/docker/nginx/dist.tar.gz --output /usr/share/nginx/html/dist.tar.gz && \
+    cd /usr/share/nginx/html/ && tar -zxvf dist.tar.gz && chmod +x /usr/bin/pre-init /usr/bin/create-admin && \
+    apk del --no-cache curl tar && rm -rf /var/cache/apk/* /usr/share/nginx/html/dist.tar.gz
+
+VOLUME ["/config"]
+
+EXPOSE 80 443
+
+CMD ["/usr/bin/supervisord", "-c", "/etc/supervisord.conf"]
---
+++ files/pre-init.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+set -e
+set -u
+
+APP="/config/application.properties"
+APPYAML="/config/application.yaml"
+#
+# Verificação do banco de dados.
+#
+if [[ $DB_CHECK -eq "1" ]]; then
+    echo "Verificando banco de dados..."
+    export PGPASSWORD=$DB_PASSWORD
+
+    CREATE_SCHEMA="psql -U $DB_USER -h $DB_HOST $DB_NAME"
+
+$CREATE_SCHEMA <<SQL
+CREATE SCHEMA IF NOT EXISTS gestaoriscos AUTHORIZATION ${DB_USER};
+CREATE SCHEMA IF NOT EXISTS aud_gestaoriscos AUTHORIZATION ${DB_USER};
+ALTER USER ${DB_USER} SET search_path=gestaoriscos,aud_gestaoriscos,public;
+SQL
+fi
+
+#
+# Verificação de sobreposição dos arquivos de configuração.
+#
+if [[ -f $APP ]]; then
+    sed -i "/spring.datasource.url/c\spring.datasource.url=jdbc:postgresql://$DB_HOST:5432/$DB_NAME" $APP
+    sed -i "/spring.datasource.username/c\spring.datasource.username=$DB_USER" $APP
+    sed -i "/spring.datasource.password/c\spring.datasource.password=$DB_PASSWORD" $APP
+else
+    echo "ERRO !!!"
+    echo "NÃO FOI POSSIVEL ENCONTRAR O ARQUIVO DE CONFIGURAÇÃO application.properties"
+    exit 1
+fi
+if [[ -f $APPYAML ]]; then
+    sed -i "/client-id/c\client-id: $ECIDADAO_ID" $APPYAML
+    sed -i "/client-secret/c\client-secret: $ECIDADAO_SECRET" $APPYAML
+    sed -i "/pre-established-redirect-uri/c\pre-established-redirect-uri: https://$ECIDADAO_SERVER/gestaoriscos/api/login/openid" $APPYAML
+    sed -i "/dominio/c\dominio: $ECIDADAO_SERVER" $APPYAML
+    sed -i "/redireciona-login-sucesso/c\redireciona-login-sucesso: https://$ECIDADAO_SERVER" $APPYAML
+    sed -i "/redireciona-logout-sucesso/c\redireciona-logout-sucesso:  https://$ECIDADAO_SERVER" $APPYAML
+
+    if [[ $DB_CHECK -eq "PROD"]]; then
+        sed -i "/access-token-uri/c\access-token-uri: https://scp.brasilcidadao.gov.br/scp/token" $APPYAML
+        sed -i "/user-authorization-uri/c\user-authorization-uri: https://scp.brasilcidadao.gov.br/scp/authorize" $APPYAML
+    else
+        sed -i "/access-token-uri/c\access-token-uri: https://testescp-ecidadao.estaleiro.serpro.gov.br/scp/token" $APPYAML
+        sed -i "/user-authorization-uri/c\user-authorization-uri: https://testescp-ecidadao.estaleiro.serpro.gov.br/scp/authorize" $APPYAML
+    fi
+else
+    echo "ERRO !!!"
+    echo "NÃO FOI POSSIVEL ENCONTRAR O ARQUIVO DE CONFIGURAÇÃO application.YAML"
+    exit 1
+fi
+
+#
+# Inicia o Java Server
+#
+/usr/bin/java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /app.jar
